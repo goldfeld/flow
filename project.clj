df011a1826e7601e465697c8aa9b4180ac54eab1
Vic Goldfeld <vic@longstorm.org>
2015-04-05 21:01:14 -0300

    update all dependencies to use new node repl

--- project.clj
+++ project.clj
@@ -3,24 +3,27 @@
   :url "http://github.com/goldfeld/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-npm "0.4.0"]
-            [com.cemerick/clojurescript.test "0.3.0"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-npm "0.5.0"]
+            [com.cemerick/clojurescript.test "0.3.3"]]
   :test-paths ["test"]
   :aliases {"cleantest" ["do" "clean," "cljsbuild" "once," "test,"]
             "autotest" ["do" "clean," "cljsbuild" "auto" "test"]}
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [prismatic/schema "0.2.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3165"]
+                 [prismatic/schema "0.4.0"]]
   :node-dependencies [[phantomjs "1.9.x"]]
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src" "test"]
                         :notify-command ["phantomjs" :cljs.test/runner
                                          "target/flow_test.js"]
-                        :compiler {:libs [""]
-                                   :output-to "target/flow_test.js"
+                        :compiler {:output-to "target/flow_test.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]
               :test-commands {"unit-tests" ["phantomjs" :runner
-                                            "target/flow_test.js"]}})
+                                            "target/flow_test.js"]}}
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.0"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware
+                                  [cemerick.piggieback/wrap-cljs-repl]}}})
